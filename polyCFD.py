934d01373dfa26b1fd86d57dab5331314f52122d
Seth <cornfreegorn@gmail.com>
2020-12-07 18:48:09 -0500

    Create polyCFD.py

---
+++ polyCFD.py
@@ -0,0 +1,122 @@
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+
+draw_n=False
+dx = 0.1
+dy = 0.1
+nx = 30
+ny = 30
+count = nx*ny
+sigma = 0.2
+
+dt=sigma*dx
+
+
+u=np.ones(nx*ny)
+v=np.ones(nx*ny)
+
+u[int(ny*nx/2)+int(nx/2)-2:int(ny*nx/2)+int(nx/2)+2]=-2
+v[int(ny*nx/2)+int(nx/2)-2:int(ny*nx/2)+int(nx/2)+2]=2
+
+pos = np.zeros((nx*ny,2))
+
+neighbors = {}
+
+maxT = 50
+Frames=[]
+
+def getds(a,b):
+    return pos[a,:]-pos[b,:]
+
+for y in np.arange(0,ny):
+    for x in np.arange(0,nx):
+        pos[x+y*ny,0]=x*dx+(y%2)*dx*0.5
+        pos[x+y*ny,1]=y*dy
+for y in np.arange(0,ny):
+    for x in np.arange(0,nx):
+        idx=x+nx*y
+        neighbors[idx]=[]
+        #Horizontal neighbors
+        if x > 0:
+            neighbors[idx].append(idx-1)
+        if x < nx-1:
+            neighbors[idx].append(idx+1)
+        #Up neighbors
+        if y < ny-1:
+            neighbors[idx].append(idx+nx)
+            if y%2==1:
+                if x < nx-1:
+                    neighbors[idx].append(idx+nx+1)
+            else:
+                if x > 0:
+                    neighbors[idx].append(idx+nx-1)
+        #down neighbors
+        if y > 0:
+            neighbors[idx].append(idx-nx)
+            if y%2==1:
+                if x < nx-1:
+                    neighbors[idx].append(idx-nx+1)
+            else:
+                if x > 0:
+                    neighbors[idx].append(idx-nx-1)
+if draw_n:
+    fig=px.scatter(x=pos[:,0],y=pos[:,1])
+    for t in np.arange(0,nx*ny):
+        for n in neighbors[t]:
+            fig.add_shape(type="line", x0=pos[t,0],y0=pos[t,1],x1=pos[n,0],y1=pos[n,1])
+    fig.show()
+
+    
+    
+startData = go.Contour(z=u+v,x=pos[:,0],y=pos[:,1])
+
+for t in np.arange(0,maxT):
+    #print(str(100*t/maxT)+"% done")
+    un=u.copy()
+    vn=v.copy()
+    for p in np.arange(0,count):
+        dudx=0
+        dudy=0
+        dvdx=0
+        dvdy=0
+        for n in neighbors[p]:
+            ds=getds(p,n)
+            if ds[0]!=0:
+                dudx+=(un[p]-un[n])/ds[0]
+                dvdx+=(vn[p]-vn[n])/ds[0]
+               
+            if ds[1]!=0:
+                dudy+=(un[p]-un[n])/ds[1]
+                dvdy+=(vn[p]-vn[n])/ds[1]
+        ncount=len(neighbors[p])
+        dudx/=ncount
+        dudy/=ncount
+        dvdx/=ncount
+        dvdy/=ncount
+        u[p]=un[p]-un[p]*dt*dudx-vn[p]*dt*dudy
+        v[p]=vn[p]-un[p]*dt*dvdx-vn[p]*dt*dvdy
+
+    
+    
+    Frames.append(go.Frame(data=go.Contour(z=u+v,x=pos[:,0],y=pos[:,1], 
+                                           contours_coloring='heatmap', 
+                                           line_width=0)))
+
+    
+fig = go.Figure(
+    data=startData,
+    layout=go.Layout(
+        xaxis=dict(range=[0, 5], autorange=False),
+        yaxis=dict(range=[0, 5], autorange=False),
+        title="Pressure",
+        updatemenus=[dict(
+            type="buttons",
+            buttons=[dict(label="Play",
+                          method="animate",
+                          args=[None])])]
+    ),
+    frames=Frames)
+  
+    
+fig.show()
